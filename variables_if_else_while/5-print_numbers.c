3d3fefeaa8f61d874634874191f683b040a6efa4
Mel <spathium@DESKTOP-18LINP4>
2022-10-24 12:39:13 -0500

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Print numbers
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+		int ent;
+
+		for (ent = 0; ent <= 9; ent++)
+		printf("%d", ent);
+		printf("\n");
+
+		return (0);
+}
