b04fb056df96a3085829d90ac471eba4a43f3d70
Mel <spathium@DESKTOP-18LINP4>
2022-10-25 23:56:18 -0500

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -14,7 +14,7 @@ void times_table(void)
 
 	for (j = 0; j <= 9; j++)
 	{
-	
+
 		for (i = 0; i <= 9; i++)
 		{
 
@@ -35,7 +35,7 @@ void times_table(void)
 				_putchar(n / 10 + '0');
 				_putchar(n % 10 + '0');
 			}
-	
+
 			if (i < 9)
 			{
 				_putchar(',');
